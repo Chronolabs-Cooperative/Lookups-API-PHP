14c8a254ca74ceb3f10112dc885fe24a3d3e825b
Dr. Xaies <mynamesnot@2240>
2017-12-27 19:46:56 +1100

    Mass Pushing Shell Bash Script...

---
+++ crons/cron.updategeoip.sh
@@ -0,0 +1,15 @@
+## BASH !! SHELL BEACH !!
+mkdir /usr/share/GeoIP
+cd /usr/share/GeoIP
+rm *
+echo "Downloading GEOIP Database Resources!\n"
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIP.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPASNum.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPCity.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPDomain.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPISP.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPNetSpeedCell.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPOrg.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPRegion.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPv6.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoLiteCityIPv6.dat
