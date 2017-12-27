ebffb9c10dc62b5fb70ed78ec23ff609b7eedfda
Dr. Xaies <mynamesnot@2240>
2017-12-27 19:45:01 +1100

    Mass Pushing Shell Bash Script...

--- crons/cron.updategeoip.sh
+++ crons/cron.updategeoip.sh
@@ -1,1 +1,15 @@
-<script>history.go(-1);</script>
+## BASH !! SHELL BEACH !!
+mkdir /var/share/geoip
+cd /var/share/geoip
+rm *
+echo "Downloading GEOIP Database Resources!\n"
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIP.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPASNum.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPCity.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPDomain.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPISP.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPNetSpeedCell.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPOrg.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPRegion.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoIPv6.dat
+wget https://github.com/maxmind/geoip-api-php/raw/master/tests/data/GeoLiteCityIPv6.dat
