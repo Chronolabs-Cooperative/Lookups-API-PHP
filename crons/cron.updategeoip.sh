bf716eb809e6d33aa03bc86d0e033403be3bcd1c
Dr. Xaies <mynamesnot@2240>
2017-12-27 20:36:27 +1100

    Mass Pushing Shell Bash Script...

--- crons/cron.updategeoip.sh
+++ crons/cron.updategeoip.sh
@@ -58,12 +58,12 @@ gunzip -c $download_path/GeoIPDomain.dat.gz > $geolite_path/GeoIPDomain.dat
 rm -f $download_path/GeoIPDomain.dat.gz
 
 $prg https://github.com/gsmlg/GeoIP.dat/raw/master/GeoIPCity.dat.gz
-if [ ! -e $download_path/GeoIPISP.dat.gz ]; then
-        echo "Unable to find GeoIPISP.dat.gz!"
+if [ ! -e $download_path/GeoIPCity.dat.gz ]; then
+        echo "Unable to find GeoIPCity.dat.gz!"
         exit 1
 fi
-gunzip -c $download_path/GeoIPISP.dat.gz > $geolite_path/GeoIPISP.dat
-rm -f $download_path/GeoIPISP.dat.gz
+gunzip -c $download_path/GeoIPCity.dat.gz > $geolite_path/GeoIPCity.dat
+rm -f $download_path/GeoIPCity.dat.gz
 
 $prg http://geolite.maxmind.com/download/geoip/database/GeoIPNetSpeedCell.dat.gz
 if [ ! -e $download_path/GeoIPNetSpeedCell.dat.gz ]; then
